44303b7428b80679bb180a3d93c65449392502b1
George Samuel <geosam2708@gmail.com>
2023-04-07 04:57:23 +0300

    added click listener to buttons

--- js/index.js
+++ js/index.js
@@ -6,6 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let searchBar = document.querySelector('input#searchText')
     let searchBtn = document.querySelector('button#searchBtn')
     let unoderedList = document.querySelector('ul#countryList')
+    let singleDetailed = document.querySelector('div#singleInformation')
 
     fetchData("https://wft-geo-db.p.rapidapi.com/v1/geo/countries?limit=10", {
         method : "GET",
@@ -16,6 +17,8 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }).then(result => {
         for(const country of result.data){
+
+            //iterate over the object and manipulate DOM
             let singleCountry =document.createElement('div')
             singleCountry.className = "singleInfo"
             singleCountry.innerHTML = `
@@ -38,6 +41,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
             `
             unoderedList.appendChild(singleCountry)
+
+            //add eventListener to the moredetails button
+            let moreDetails = document.querySelector('button#moredetails.respbtns')
+            moreDetails.addEventListener('click', () => {
+                renderDetails(country, singleDetailed)
+            })
         }
     })
 })
@@ -46,4 +55,33 @@ document.addEventListener('DOMContentLoaded', () => {
 async function fetchData(url, obj){
     const response = fetch(url, obj).then(result => result.json())
     return response
+}
+
+//render detailed info based on information passed
+function renderDetails(data,element){
+    fetchData(`https://wft-geo-db.p.rapidapi.com/v1/geo/countries/${data.code}`, { method : "GET",
+    headers : {
+        'X-RapidAPI-Key' : 'a0760444a3msh119cce59cf8be7fp1a083bjsn90c151981805',
+        'X-RapidAPI-Host' : 'wft-geo-db.p.rapidapi.com',
+        'accept' : 'application/json'
+    }
+}).then(resp => {
+    element.innerHTML = `
+    <img src= ${resp.data.flagImageUri} width="200px" height="180px" alt="Flag Image">
+		<div class="cntryFamily">
+                        <label for="calling code">Calling code:</label>
+                        <span class="info">${resp.data.callingCode}</span>
+                    </div>
+                    
+                    <div class="cntryFamily">
+                        <label for="cntryname">Country Name:</label>
+                        <span class="info">${resp.data.name}</span>
+                    </div>
+                    
+                    <div class="cntryFamily">
+                        <label for="numRegions">Number of Regions</label>
+                        <span class="info">${resp.data.numRegions}</span>
+                    </div>
+    `
+})
 }
